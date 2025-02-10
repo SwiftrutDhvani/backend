0bf5b99101967efcc7cc5af016217bb906a2def8
SwiftrutDhvani <dhvani.barot@swiftrut.com>
2025-02-11 04:47:08 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "Ramesh", email: "ramesh@gmail.com" },
+  { name: "Dhvani", email: "dhvani@gmail.com" },
+  { name: "Mayur", email: "mayur@gmail.com" },
+];
+
+module.exports = data;
